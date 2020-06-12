e2cc7fb1d977bd638b29ff10b180856c74e24de9
Janani NA <janani2285@gmail.com>
2020-06-11 18:11:56 -0700

    Added validator code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,20 +5,14 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 //Function to generate random password
 function generatePassword() {
 
   //Getting user input for pasword criteria
-  var pwdLength = prompt("Enter length of the password. Min: 8, Max:128 ");
-  var isLowerCase = confirm("Do you want to include lower case alphabets in your password? ");
-  var isUpperCase = confirm("Do you want to include upper case alphabets in your password? ");
-  var isNumeric = confirm("Do you want to include numbers in your password? ");
-  var isSpecial = confirm("Do you want to include special characters in your password? ");
+  var pwdLength = prompt("How many charaters should your password consists of?");
 
   var pwdCriteriaString = "";
   var password = "";
@@ -28,35 +22,32 @@ function generatePassword() {
   var numeric = "0123456789";
   var specialChar = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+  if (pwdLength < 8 || pwdLength > 128) {
+    alert("Password cannot be generated. Required minimum characters: 8 , Maximum characters:128");
+  }else {
+    var isLowerCase = confirm("Do you want to include lower case alphabets in your password?");
+    var isUpperCase = confirm("Do you want to include upper case alphabets in your password?");
+    var isNumeric = confirm("Do you want to include numbers in your password?");
+    var isSpecial = confirm("Do you want to include special characters in your password?");
 
-  pwdCriteriaString = criteriaStringGenerator(isLowerCase, lowerCase, pwdCriteriaString);
-  pwdCriteriaString = criteriaStringGenerator(isUpperCase, upperCase, pwdCriteriaString);
-  pwdCriteriaString = criteriaStringGenerator(isNumeric, numeric, pwdCriteriaString);
-  pwdCriteriaString = criteriaStringGenerator(isSpecial, specialChar, pwdCriteriaString);
-
-
-  for (var i = 1; i <= pwdLength; i++) {
-    var char = Math.floor(Math.random() * pwdCriteriaString.length + 1);
-    password += pwdCriteriaString.charAt(char);
+    if(isLowerCase || isUpperCase || isNumeric || isSpecial){
+      
+      pwdCriteriaString = criteriaStringGenerator(isLowerCase, lowerCase, pwdCriteriaString);
+      pwdCriteriaString = criteriaStringGenerator(isUpperCase, upperCase, pwdCriteriaString);
+      pwdCriteriaString = criteriaStringGenerator(isNumeric, numeric, pwdCriteriaString);
+      pwdCriteriaString = criteriaStringGenerator(isSpecial, specialChar, pwdCriteriaString);
+      
+      for (var i = 1; i <= pwdLength; i++) {
+       
+        var char = Math.floor(Math.random() * pwdCriteriaString.length + 1);
+        password += pwdCriteriaString.charAt(char);
+        
+      }
+    }else{
+      alert("Password cannot be generated. None of the criteria (lower case,upper case,numeric, special character) were selected.");
+    }  
   }
-
-  /*var isBlank = isBlankValidator(pwdLength);
-  if(isBlank){
-   pwdLength = prompt("Enter alength of the password. Min: 8, Max:128 ");
-  }*/
-
   return password;
-}
-
-//A validator function to check if user input is empty or not
-function isBlankValidator() {
-
-  return false;
-}
-
-function charTypeValidator() {
-
-  return false;
 }
 
 function criteriaStringGenerator(isCriteria, criteriaString, pwdCriteriaString) {
